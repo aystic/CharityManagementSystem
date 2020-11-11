188130e27d0834e90f6cfccee1e238e9348dac32
Mohit Pathak <pmohitb3011@gmail.com>
2020-11-12 00:09:21 +0530

    Added the popup messages to the signin page for admin and NGO login

--- login/signin.js
+++ login/signin.js
@@ -1,22 +1,32 @@
 let message = document.getElementsByClassName("message")[0];
 message.classList.add("invisible");
+let messagebtn=document.getElementById("messagebtn");
 let email = document.getElementsByClassName("emailbox")[0];
 let pwd = document.getElementsByClassName("pwdbox")[0];
 let btn = document.getElementsByClassName("signupbtn")[0];
 let usernames = ["admin@admin.com", "test@test.com"];
 let passwords = ["admin", "test"];
-btn.addEventListener("click", function () {
-  if (email.value == usernames[0] && pwd.value == passwords[0]) {
-      // document.getElementsByClassName("loginmessage")[0].textContent ="Login as ADMIN.";
-      // message.classList.remove("invisible");
-      alert("Login as Admin!");
-      document.getElementById('form').setAttribute("action","../adminindex.html")
+btn.addEventListener("click",function(){
+  if(email.value == usernames[0] && pwd.value == passwords[0]) {
+    document.getElementsByClassName("loginmessage")[0].textContent="Login as ADMIN.";
+    message.classList.remove("invisible");
+    messagebtn.addEventListener("click",function(){
+        window.location.replace("../adminindex.html");
+    });
 
-
-  } else if (email.value == usernames[1] && pwd.value == passwords[1]) {
-    alert("Login as NGO!");
-    document.getElementById("form").setAttribute("action", "../ngoindex.html");
-
-    // window.location.replace('../ngoindex.html');
-  }
+   } else if (email.value == usernames[1] && pwd.value == passwords[1]) {
+    document.getElementsByClassName("loginmessage")[0].textContent="Login as NGO.";
+    message.classList.remove("invisible");
+    messagebtn.addEventListener("click",function(){
+      window.location.replace("../ngoindex.html");
+  });
+   }
 });
+// function redirect(usertype){
+//   if(usertype=="admin"){
+//     window.location.replace("../adminindex.html");
+//   }
+//   else{
+//     window.location.replace("../ngoindex.html");
+//   }
+// }
