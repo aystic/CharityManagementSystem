e41b2a9d39c879b518a6bf26b637134f379ce352
Mohit Pathak <pmohitb3011@gmail.com>
2020-11-13 04:20:12 +0530

    Added functionality to the homepage's buttons

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-
 let scrollinpx=window.scrollY;
 let btn=document.getElementById('gototop');
 btn.classList.add("invisible");
@@ -12,3 +11,61 @@ function myscrollfunction(){
     }
 }
 
+//LOCALSTORAGE SECTION
+let storageavailable=false;
+if(!JSON.parse(localStorage.storageavailable)){
+    localStorage.setItem("storageavailable","true");
+    localStorage.setItem("isloggedin",false);
+    localStorage.setItem("loggedinas","");
+}
+
+// hiding and showing the signoutbutton
+document.getElementById("isloggedin").classList.add("invisible");
+document.getElementById("accountbtn").classList.add("invisible");
+
+if(JSON.parse(localStorage.isloggedin)){
+    if(localStorage.loggedinas=="individual"){
+        document.getElementById("isloggedin").classList.remove("invisible");
+        document.getElementById("accountbtn").classList.remove("invisible");
+        document.getElementById("requestbutton").classList.add("invisible");
+        document.getElementById("tosignin").classList.add("invisible");
+    }
+    if(localStorage.loggedinas=="ngo"){
+        document.getElementById("isloggedin").classList.remove("invisible");
+        document.getElementById("accountbtn").classList.remove("invisible");
+        document.getElementById("donatebtn").classList.add("invisible");
+        document.getElementById("tosignin").classList.add("invisible");
+    }
+}
+
+
+// buttonfunctions
+function donateredirect(){
+    if(JSON.parse(localStorage.isloggedin)){
+        window.location.replace("/HTML/donate.html");
+    }else{
+        window.location.replace("/HTML/signin.html");
+    }
+}
+function requestfordonationredirect(){
+    if(JSON.parse(localStorage.isloggedin)){
+        window.location.replace("/HTML/requestfordonation.html");
+    }else{
+        window.location.replace("/HTML/signin.html");
+    }
+}
+function signout(){
+    localStorage.isloggedin=false;
+    localStorage.loggedinas="";
+    window.location.replace("/HTML/index.html");
+}
+function signup(){
+    window.location.replace("/HTML/signin.html");
+}
+function account(){
+    if(localStorage.loggedinas=="individual"){
+        window.location.replace("/HTML/individualaccount.html");
+    }else{
+        window.location.replace("/HTML/ngoaccount.html");
+    }
+}
--- JS/signup.js
+++ JS/signup.js
@@ -1,3 +1,51 @@
+let message = document.getElementsByClassName("message")[0];
+message.classList.add("invisible");
+let messagebtn=document.getElementById("messagebtn");
+document.getElementById("form").submit(function(e) {
+        e.preventDefault();
+});
+
+
+// btn.addEventListener("click",function(){
+//         if(email.value == usernames[0] && pwd.value == passwords[0]) {
+//           document.getElementsByClassName("loginmessage")[0].textContent="Login as ADMIN.";
+//           message.classList.remove("invisible");
+//           messagebtn.addEventListener("click",function(){
+//             redirect("admin");
+//         });
+//          } else if (email.value == usernames[1] && pwd.value == passwords[1]) {
+//           document.getElementsByClassName("loginmessage")[0].textContent="Login as NGO.";
+//           message.classList.remove("invisible");
+//           messagebtn.addEventListener("click",function(){
+//             redirect("ngo");
+//         });
+//          }else{
+//           document.getElementById("successmsg").textContent="Failure!";
+//           document.getElementById("msgimage").setAttribute("src","/IMAGES/cross.png");
+//           document.getElementById("messagebtn").textContent="Try Again";
+//           document.getElementsByClassName("loginmessage")[0].innerHTML="Invalid Credentials!";
+//           message.classList.remove("invisible");
+//           messagebtn.addEventListener("click",function(){
+//           redirect("error");
+//          });
+//         }
+//       });
+//       function redirect(usertype){
+//         if(usertype=="admin"){
+//           window.location.replace("/HTML/adminindex.html");
+//         }
+//         else if(usertype=="ngo"){
+//           window.location.replace("/HTML/ngoindex.html");
+//         }else if(usertype=="error"){
+//           window.location.replace("/HTML/signin.html");
+      
+//         }
+//       }
+
+
+
+
+
 let btn=document.getElementsByClassName('signupbtn')[0]
 btn.click();
 btn.addEventListener('click',function(){
