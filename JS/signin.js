07eecc8744c456d03b98412e9a9376cb73b8ea23
Mohit Pathak <pmohitb3011@gmail.com>
2020-11-16 05:02:57 +0530

    Updated the storage to be array based Updated the signup and signin pages to use this array based storage

--- JS/signin.js
+++ JS/signin.js
@@ -9,50 +9,135 @@ messagebg.classList.add("invisible");
 let email = document.getElementsByClassName("emailbox")[0];
 let pwd = document.getElementsByClassName("pwdbox")[0];
 let btn = document.getElementsByClassName("signupbtn")[0];
-btn.click();  
-let usernames = ["admin@admin.com", "test@test.com"];
-let passwords = ["admin", "test"];
+let count=0;
+btn.click();
 document.getElementById("form").submit(function(e) {
   e.preventDefault();
 });
-btn.addEventListener("click",function(){
-  if(email.value == usernames[0] && pwd.value == passwords[0]) {
-    document.getElementsByClassName("loginmessage")[0].textContent="Login as ADMIN.";
-    messagebg.classList.remove("invisible");
-    message.classList.remove("invisible");
-    messagebtn.addEventListener("click",function(){
-      redirect("admin");
-  });
-   } else if (email.value == usernames[1] && pwd.value == passwords[1]) {
-    document.getElementsByClassName("loginmessage")[0].textContent="Login as NGO.";
-    messagebg.classList.remove("invisible");
-    message.classList.remove("invisible");
-    messagebtn.addEventListener("click",function(){
-      redirect("ngo");
-  });
-   }else if(email.value!=""){
-    document.getElementById("successmsg").textContent="Failure!";
-    document.getElementById("msgimage").setAttribute("src","/IMAGES/cross.png");
-    document.getElementById("messagebtn").textContent="Try Again";
-    document.getElementsByClassName("loginmessage")[0].innerHTML="Invalid Credentials!";
-    messagebg.classList.remove("invisible");
-    message.classList.remove("invisible");
-    messagebtn.addEventListener("click",function(){
-    redirect("error");
-   });
-  }
-});
-function redirect(usertype){
-  if(usertype=="admin"){
-    window.location.replace("/HTML/adminindex.html");
-  }
-  else if(usertype=="ngo"){
-    window.location.replace("/HTML/ngoindex.html");
-  }else if(usertype=="error"){
-    window.location.replace("/HTML/signin.html");
 
+//getting all the users in an array
+let users=[];
+let newuser=[];
+for(let i=0;i<localStorage.length;i++){
+  if(localStorage.key(i).slice(0,4)=="user"){
+    users.push(localStorage.getItem(localStorage.key(i)));
   }
 }
+
+
+btn.addEventListener("click",function(){
+  if(email.value =="admin@admin.com" && pwd.value =="admin") {
+        document.getElementsByClassName("loginmessage")[0].textContent="Login as ADMIN.";
+        messagebg.classList.remove("invisible");
+        message.classList.remove("invisible");
+        messagebtn.addEventListener("click",function(){
+          redirect("admin");
+      });
+    }else{
+      for(let i=0;i<users.length;i++){
+        if(email.value==users[i].split(",")[0] && pwd.value==users[i].split(",")[1]){
+          if(users[i].split(",")[2]=="false"){//ngo
+            localStorage.isloggedin=true;
+            localStorage.loggedinas="ngo";
+            document.getElementsByClassName("loginmessage")[0].textContent="Login as NGO.";
+            messagebg.classList.remove("invisible");
+            message.classList.remove("invisible");
+            messagebtn.addEventListener("click",function(){
+              // redirect("ngo");
+              window.location.replace("/HTML/index.html");
+            });
+          }else{
+            localStorage.isloggedin=true;
+            localStorage.loggedinas="individual";
+            document.getElementsByClassName("loginmessage")[0].textContent="Login as Donor";
+            messagebg.classList.remove("invisible");
+            message.classList.remove("invisible");
+            messagebtn.addEventListener("click",function(){
+              // redirect("individual");
+              window.location.replace("/HTML/index.html");
+            });
+          }
+        }else{
+          count++;
+        } 
+      }
+      if(count==users.length){
+        document.getElementById("successmsg").textContent="Failure!";
+        document.getElementById("msgimage").setAttribute("src","/IMAGES/cross.png");
+        document.getElementById("messagebtn").textContent="Try Again";
+        document.getElementsByClassName("loginmessage")[0].innerHTML="Invalid Credentials!";
+        messagebg.classList.remove("invisible");
+        message.classList.remove("invisible");
+        messagebtn.addEventListener("click",function(){
+        redirect("error");
+      });
+    }
+  }
+   
+  });
+
+
+// function redirect(usertype){
+//   if(usertype=="admin"){
+//     window.location.replace("/HTML/adminindex.html");
+
+//   }
+//   else if(usertype=="ngo"){
+//     if(localStorage.getItem("detailssubmitted")=="false"){
+//       window.location.replace("/HTML/ngoindex.html");
+//     }else{
+      
+
+//     }
+//   }else if(usertype=="individual"){
+//     if(localStorage.getItem("detailssubmitted")=="false"){
+//       window.location.replace("/HTML/individualindex.html");
+//     }else{
+//       window.location.replace("/HTML/index.html");
+//     }
+//   }
+// }
+
 function redirecttosignuppage(){
   window.location.replace("/HTML/signup.html");
 }
+
+
+
+
+// else{
+//   for (let i = 0;i < localStorage.length;i++) {
+//     if ("username" == localStorage.key(i).slice(0, 8)) {
+//       //console.log(localStorage.getItem(localStorage.key(i)));
+//       if (localStorage.getItem(localStorage.key(i)) == email.value && localStorage.getItem("password"+(localStorage.key(i)).slice(8,9)==pwd.value)) {
+//         count++;
+//         if(localStorage.getItem("isindividual"+(localStorage.key(i)).slice(8,9))=="false"){//ngo
+//           document.getElementsByClassName("loginmessage")[0].textContent="Login as NGO.";
+//           messagebg.classList.remove("invisible");
+//           message.classList.remove("invisible");
+//           messagebtn.addEventListener("click",function(){
+//             redirect("ngo");
+//            });
+//         }
+//           else{//individual
+//             document.getElementsByClassName("loginmessage")[0].textContent="Login as Donor.";
+//             messagebg.classList.remove("invisible");
+//             message.classList.remove("invisible");
+//             messagebtn.addEventListener("click",function(){
+//             redirect("individual");
+//             });
+//           }
+//       }
+//      } else if(count==JSON.parse(localStorage.getItem("usercount"))){
+//           document.getElementById("successmsg").textContent="Failure!";
+//           document.getElementById("msgimage").setAttribute("src","/IMAGES/cross.png");
+//           document.getElementById("messagebtn").textContent="Try Again";
+//           document.getElementsByClassName("loginmessage")[0].innerHTML="Invalid Credentials!";
+//           messagebg.classList.remove("invisible");
+//           message.classList.remove("invisible");
+//           messagebtn.addEventListener("click",function(){
+//           redirect("error");
+//          });
+//      }
+//   }
+// }
--- JS/signup.js
+++ JS/signup.js
@@ -22,24 +22,86 @@ let pwd = document.getElementsByClassName("input-box")[1];
 let isindividual = document.getElementById("Individual");
 let isngo = document.getElementById("NGO");
 let documentverified = false;
+let detailssubmitted=false;
 let terms = document.getElementById("termsbox");
 let alreadyauser=false;
 let usercount = JSON.parse(localStorage.getItem("usercount"));
 usercount++;
 
+//creating an array of existing users
+let users=[];
+let newuser=[];
+for(let i=0;i<localStorage.length;i++){
+  if(localStorage.key(i).slice(0,4)=="user"){
+    users.push(localStorage.getItem(localStorage.key(i)));
+  }
+}
+
 //submission of form
 submitbtn.addEventListener("click", function () {
-  for (
-    let i = 0;
-    i < (4 * JSON.parse(localStorage.getItem("usercount"))) + 1;
-    i++
-  ) {
-    if ("username" == localStorage.key(i).slice(0, 8)) {
-      //console.log(localStorage.getItem(localStorage.key(i)));
-      if (localStorage.getItem(localStorage.key(i)) == email.value) {
+  for(let i=0;i<users.length;i++){
+    if(users[i].split(",")[0]==email.value){
+      message.classList.remove("invisible");
+      messagebg.classList.remove("invisible");
+      alreadyauser=true;
+      message.classList.remove("invisible");
+      messagebg.classList.remove("invisible");
+      document.getElementById("msgimage").setAttribute("src", "/IMAGES/caution.png");
+      document.getElementById("successmsg").textContent = "Caution!";
+      document.getElementsByClassName("loginmessage")[0].textContent ="Email ID Already Registered.";
+      document.getElementById("messagebtn").addEventListener("click",function(){
+          // console.log("Email ID Already registered! ");
+          message.classList.add("invisible");
+          messagebg.classList.add("invisible");
+          alreadyauser=false;
+          document.getElementById("form").reset();
+      });
+    }
+  }
+  //new user
+  if (
+        email.value != "" &&
+        pwd.value != "" &&
+        (isindividual.checked || isngo.checked) &&
+        terms.checked &&
+        alreadyauser==false
+      ) {
+        // storingthevaluesinlocalstorage
+        newuser.push(email.value,pwd.value,isindividual.checked,detailssubmitted,documentverified);
+        localStorage.setItem("user"+usercount,newuser)
+        localStorage.setItem("usercount", usercount);
         message.classList.remove("invisible");
         messagebg.classList.remove("invisible");
-        alreadyauser=true;
+        document
+          .getElementById("msgimage")
+          .setAttribute("src", "/IMAGES/check.png");
+        document.getElementById("successmsg").textContent = "Success!!";
+        document.getElementsByClassName("loginmessage")[0].textContent =
+          "SignUp Successfull.";
+        document
+          .getElementById("messagebtn")
+          .addEventListener("click", function () {
+            message.classList.add("invisible");
+            messagebg.classList.add("invisible");
+            if (isindividual.checked){
+              localStorage.isloggedin=true;
+              localStorage.loggedinas="individual"
+              window.location.replace("/HTML/individualindex.html");
+            }
+            else {
+              localStorage.isloggedin=true;
+              localStorage.loggedinas="ngo"
+              window.location.replace("/HTML/ngoindex.html");
+            }
+          });
+      } else if (
+        email.value == "" &&
+        pwd.value == "" &&
+        isindividual.checked == false &&
+        isngo.checked == false &&
+        terms.checked == false &&
+        alreadyauser==false
+      ) {
         message.classList.remove("invisible");
         messagebg.classList.remove("invisible");
         document
@@ -47,112 +109,36 @@ submitbtn.addEventListener("click", function () {
           .setAttribute("src", "/IMAGES/caution.png");
         document.getElementById("successmsg").textContent = "Caution!";
         document.getElementsByClassName("loginmessage")[0].textContent =
-          "Email ID Already Registered.";
+          "Please fill the details first.";
         document
           .getElementById("messagebtn")
-          .addEventListener("click",function(){
-            // console.log("Email ID Already registered! ");
+          .addEventListener("click", function () {
             message.classList.add("invisible");
             messagebg.classList.add("invisible");
-            alreadyauser=false;
+            // window.location.replace("/HTML/signup.html");
+            document.getElementById("form").reset();
+          });
+      } else if(alreadyauser==false){
+        message.classList.remove("invisible");
+        messagebg.classList.remove("invisible");
+        document
+          .getElementById("msgimage")
+          .setAttribute("src", "/IMAGES/cross.png");
+        document.getElementById("successmsg").textContent = "Failure!!";
+        document.getElementsByClassName("loginmessage")[0].textContent =
+          "Invalid Credentials.";
+        document
+          .getElementById("messagebtn")
+          .addEventListener("click", function () {
+            message.classList.add("invisible");
+            messagebg.classList.add("invisible");
+            // window.location.replace("/HTML/signup.html");
             document.getElementById("form").reset();
           });
       }
-    }
-  }
-  //for(let i=0;i<((4*usercount)+1);i++){
-  //if(localStorage.key(i).slice()){
-  //Already a user
-  // message.classList.remove("invisible");
-  // messagebg.classList.remove("invisible");
-  // document.getElementById("msgimage").setAttribute("src","/IMAGES/caution.png");
-  // document.getElementById("successmsg").textContent="Caution!";
-  // document.getElementsByClassName("loginmessage").textContent="A user already exists with this email ID!";
-  // document.getElementById("messagebtn").addEventListener("click",function(){
-  //   message.classList.add("invisible");
-  //   messagebg.classList.add("invisible");
-  //   window.location.replace("/HTML/signup.html");
-  // });
-  //}
-
-  //new user
-  if (
-    email.value != "" &&
-    pwd.value != "" &&
-    (isindividual.checked || isngo.checked) &&
-    terms.checked &&
-    alreadyauser==false
-  ) {
-    // storingthevaluesinlocalstorage
-    localStorage.setItem("username" + usercount, email.value);
-    localStorage.setItem("password" + usercount, pwd.value);
-    localStorage.setItem("isindividual" + usercount, isindividual.checked);
-    localStorage.setItem("verification" + usercount, documentverified);
-    localStorage.setItem("usercount", usercount);
-    message.classList.remove("invisible");
-    messagebg.classList.remove("invisible");
-    document
-      .getElementById("msgimage")
-      .setAttribute("src", "/IMAGES/check.png");
-    document.getElementById("successmsg").textContent = "Success!!";
-    document.getElementsByClassName("loginmessage")[0].textContent =
-      "SignUp Successfull.";
-    document
-      .getElementById("messagebtn")
-      .addEventListener("click", function () {
-        message.classList.add("invisible");
-        messagebg.classList.add("invisible");
-        if (isindividual.checked){
-          window.location.replace("/HTML/individualindex.html");
-        }
-        else {
-        window.location.replace("/HTML/ngoindex.html");
-        }
-      });
-  } else if (
-    email.value == "" &&
-    pwd.value == "" &&
-    isindividual.checked == false &&
-    isngo.checked == false &&
-    terms.checked == false &&
-    alreadyauser==false
-  ) {
-    message.classList.remove("invisible");
-    messagebg.classList.remove("invisible");
-    document
-      .getElementById("msgimage")
-      .setAttribute("src", "/IMAGES/caution.png");
-    document.getElementById("successmsg").textContent = "Caution!";
-    document.getElementsByClassName("loginmessage")[0].textContent =
-      "Please fill the details first.";
-    document
-      .getElementById("messagebtn")
-      .addEventListener("click", function () {
-        message.classList.add("invisible");
-        messagebg.classList.add("invisible");
-        // window.location.replace("/HTML/signup.html");
-        document.getElementById("form").reset();
-      });
-  } else if(alreadyauser==false){
-    message.classList.remove("invisible");
-    messagebg.classList.remove("invisible");
-    document
-      .getElementById("msgimage")
-      .setAttribute("src", "/IMAGES/cross.png");
-    document.getElementById("successmsg").textContent = "Failure!!";
-    document.getElementsByClassName("loginmessage")[0].textContent =
-      "Invalid Credentials.";
-    document
-      .getElementById("messagebtn")
-      .addEventListener("click", function () {
-        message.classList.add("invisible");
-        messagebg.classList.add("invisible");
-        // window.location.replace("/HTML/signup.html");
-        document.getElementById("form").reset();
-      });
-  }
-});
-//});
+    });
+    //});
+  
 
 function redirecttosigninpage() {
   window.location.replace("/HTML/signin.html");
