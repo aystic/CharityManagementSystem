15b0851ae2ec466490e36cc18f5b821b19af9f57
Mohit Pathak <pmohitb3011@gmail.com>
2020-11-18 07:59:22 +0530

    Added the Donor's and NGO's Account pages

---
+++ JS/individualaccount.js
@@ -0,0 +1,28 @@
+let message = document.getElementsByClassName("message")[0];
+let messagebtn=document.getElementById("messagebtn");
+let messagebg=document.getElementById("messagebg");
+message.classList.add("invisible");
+messagebg.classList.add("invisible");
+
+if(localStorage.loggedinuser.split(",")[3]=="false"){
+  message.classList.remove("invisible");
+  messagebg.classList.remove("invisible");
+  document.getElementById("msgimage").setAttribute("src","/IMAGES/caution.png");
+  document.getElementById("successmsg").textContent="Caution!";
+  document.getElementsByClassName("loginmessage")[0].textContent="Please Submit the required documents for submission first.";
+  messagebtn.addEventListener("click",function(){
+      window.location.replace("/HTML/individualindex.html");
+  });
+} else if(localStorage.loggedinuser.split(",")[4]=="false"){
+  message.classList.remove("invisible");
+  messagebg.classList.remove("invisible");
+  document.getElementById("msgimg").setAttribute("src","/IMAGES/cross.png");
+  document.getElementById("successmsg").textContent="Access Denied!";
+  document.getElementById("loginmessage").textContent="Verification Pending.";
+  messagebtn.addEventListener("click",function(){
+      window.location.replace("/HTML/index.html");
+  });
+}
+
+
+
