3f042aa523faa92476492219eabf2de8c275d00a
Mohit Pathak <pmohitb3011@gmail.com>
2020-12-16 08:50:44 +0530

    Added the functional admin page execpt for the dashboard

--- JS/adminindexscript.js
+++ JS/adminindexscript.js
@@ -32,32 +32,27 @@ if (localStorage.loggedinas != "admin") {
     }
   };
 
-  
-  let totalusers=document.getElementById("total");
-  let ngos=document.getElementById("totalngo");
-  let donors=document.getElementById("totaldonor");
-  totalusers.textContent="Total Users = "+JSON.parse(localStorage.Totalusers);
-  ngos.textContent="NGOs = "+JSON.parse(localStorage.Totalngos);
-  donors.textContent="Donors = "+JSON.parse(localStorage.Totaldonors);
 
+  let totalusers = document.getElementById("total");
+  let ngos = document.getElementById("totalngo");
+  let donors = document.getElementById("totaldonor");
 
 
   let message = document.getElementsByClassName("message")[0];
   let messagebg = document.getElementById("messagebg");
-  let fetch = document.getElementById("messagebtn");
+  let fetch = document.getElementById("fetchbtn");
+  let btn = document.getElementById("messagebtn");
+  let initialbtn = document.getElementById("initialbutton");
+  btn.classList.add("invisible");
+  initialbtn.classList.add("invisible");
   let list = document.getElementById("list");
-
-  // message.classList.add("invisible");
-  // messagebg.classList.add("invisible");
-
-
 
   //calculation of pending requests
   let pendingRequests = 0;
   let key = [];
-  let type=[];
-  let userno=[];
-  let listdetails=[];
+  let type = [];
+  let userno = [];
+  let listdetails = [];
   for (let i = 0; i < localStorage.length; i++) {
     if (localStorage.key(i).slice(0, 4) == "user") {
       if (
@@ -66,197 +61,244 @@ if (localStorage.loggedinas != "admin") {
       ) {
         pendingRequests++;
         key.push(i);
-        if(localStorage.getItem(localStorage.key(i)).split(",")[2]=="true"){
+        if (localStorage.getItem(localStorage.key(i)).split(",")[2] == "true") {
           type.push("donor");
-        }else{
+        } else {
           type.push("ngo");
         }
-        userno.push(localStorage.key(i).slice(4,localStorage.key(i).length));
+        userno.push(localStorage.key(i).slice(4, localStorage.key(i).length));
         listdetails.push(i);
       }
     }
   }
-  localStorage.setItem("listdetails",userno);
-  let details=[];
-  let order=[];
-//getting the details
-for(let i=0;i<localStorage.length;i++){
-  if(localStorage.key(i).slice(0,11)=="Detailsuser"){
-    for(let j=0;j<pendingRequests;j++){
-      if(localStorage.key(i).slice(11,localStorage.key(i).length)==userno[j]){
-        if(type[j]=="donor"){
-          let temp=[];
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[0]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[1]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[2]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[3]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[4]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[5]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[6]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[7]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[8]);
-          temp.push("donor");
-          temp.push(i);
-          details[j]=temp;
-          order.push(j);
+  localStorage.setItem("listdetails", userno);
+  let details = [];
 
-        }else{
-          let temp=[];
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[0]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[1]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[2]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[3]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[4]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[5]);
-          temp.push(localStorage.getItem(localStorage.key(i)).split(",")[6]);
-          temp.push("");
-          temp.push("");
-          temp.push("ngo");
-          temp.push(i);
-          details[j]=temp;
-        }  
-      
+  //getting the details
+  for (let i = 0; i < localStorage.length; i++) {
+    if (localStorage.key(i).slice(0, 11) == "Detailsuser") {
+      for (let j = 0; j < pendingRequests; j++) {
+        if (
+          localStorage.key(i).slice(11, localStorage.key(i).length) == userno[j]
+        ) {
+          if (type[j] == "donor") {
+            let temp = [];
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[0]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[1]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[2]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[3]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[4]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[5]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[6]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[7]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[8]);
+            temp.push("donor");
+            temp.push(i);
+            // details[userno[j]] = temp;
+            details[j] = temp;
+          } else {
+            let temp = [];
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[0]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[1]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[2]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[3]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[4]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[5]);
+            temp.push(localStorage.getItem(localStorage.key(i)).split(",")[6]);
+            temp.push("");
+            temp.push("");
+            temp.push("ngo");
+            temp.push(i);
+            // details[userno[j]] = temp;
+            details[j] = temp;
+          }
+        }
+      }
     }
-
   }
-}
-}
 
-
-message.classList.add("invisible");
-messagebg.classList.add("invisible");
-
+  // message.classList.add("invisible");
+  // messagebg.classList.add("invisible");
 
   document.getElementsByClassName("loginmessage")[0].textContent =
     pendingRequests + " Requests Pending for Verification!";
-  // message.classList.remove("invisible");
-  // messagebg.classList.remove("invisible");
 
+  message.classList.remove("invisible");
+  messagebg.classList.remove("invisible");
 
-  fetch.onclick = function () {
-    message.classList.add("invisible");
-    messagebg.classList.add("invisible");
-    for (let i = 0; i < pendingRequests; i++) {
-      let li = document.createElement("li");
-      if (details[i][9]=="donor") {
-        //donor
-        li.innerHTML =
-        "<ol>DONOR : <ul><li>NAME : " +
-        details[i][0] + " " + details[i][1] +
-        "</li><li>ADDRESS : " +
-        details[i][2] +
-        "</li><li>CITY : " +
-        details[i][3] +
-        "</li><li>STATE : " +
-        details[i][4] +
-        "</li><li>PINCODE : " +
-        details[i][5] +
-        "</li><li>CONTACT : " +
-        details[i][6] +
-        "</li><li>AADHAR NO. : " +
-        details[i][7] +
-        "</li><li>PAN CARD NO. : " +
-        details[i][8] +
-        '</li><li><button class="documentsdownloadbtn" disabled>Download the Documents!</button><button class="acceptbtn" id="accept'+i+'" onclick="accept('+i+')">Accept!</button><button class="rejectbtn" id="reject'+i+'" onclick="reject('+i+')">Reject</button></li></ul></ol>';
-      list.append(li);
-      } else {
-        //ngo
-        li.innerHTML =
-          "<ol>NGO : <ul><li>NAME : " +
-          details[i][0] +
-          "</li><li>ADDRESS : " +
-          details[i][1] +
-          "</li><li>CITY : " +
-          details[i][2] +
-          "</li><li>STATE : " +
-          details[i][3] +
-          "</li><li>PINCODE : " +
-          details[i][4] +
-          "</li><li>CONTACT : " +
-          details[i][5] +
-          "</li><li>DIFFICULTIES FACED BY NGO : " +
-          details[i][6] +
-          '</li><li><button class="documentsdownloadbtn" disabled>Download the Documents!</button><button class="acceptbtn" id="accept'+i+'" onclick="accept('+i+')">Accept!</button><button class="rejectbtn" id="reject'+i+'" onclick="reject('+i+')">Reject</button></li></ul></ol>';
-        list.append(li);
+  if (pendingRequests == 0) {
+    fetch.classList.add("invisible");
+    btn.classList.add("invisible");
+    initialbtn.classList.remove("invisible");
+    initialbtn.addEventListener("click", function () {
+      // totalusers.textContent ="Total Users = "+ JSON.parse(localStorage.Totalusers);
+      // ngos.textContent = "NGOs = " + JSON.parse(localStorage.Totalngos);
+      // donors.textContent = "Donors = " + JSON.parse(localStorage.Totaldonors);
+
+      document.onkeydown = function (e) {
+        return true;
+      };
+      messagebg.classList.add("invisible");
+      message.classList.add("invisible");
+    });
+  }else{
+    fetch.classList.remove("invisible");
+    btn.classList.add("invisible");
+    initialbtn.classList.add("invisible");
+    fetch.onclick = function () {
+      // totalusers.textContent ="Total Users = "+ JSON.parse(localStorage.Totalusers);
+      // ngos.textContent = "NGOs = " + JSON.parse(localStorage.Totalngos);
+      // donors.textContent = "Donors = " + JSON.parse(localStorage.Totaldonors);
+
+      message.classList.add("invisible");
+      messagebg.classList.add("invisible");
+      for (let i = 0; i < pendingRequests; i++) {
+        let li = document.createElement("li");
+        if (details[i][9] == "donor") {
+          //donor
+          li.innerHTML =
+            "<ol>DONOR : <ul><li>NAME : " +
+            details[i][0] +
+            " " +
+            details[i][1] +
+            "</li><li>ADDRESS : " +
+            details[i][2] +
+            "</li><li>CITY : " +
+            details[i][3] +
+            "</li><li>STATE : " +
+            details[i][4] +
+            "</li><li>PINCODE : " +
+            details[i][5] +
+            "</li><li>CONTACT : " +
+            details[i][6] +
+            "</li><li>AADHAR NO. : " +
+            details[i][7] +
+            "</li><li>PAN CARD NO. : " +
+            details[i][8] +
+            '</li><li><button class="documentsdownloadbtn" disabled>Download the Documents!</button><button class="acceptbtn" id="accept' +
+            i +
+            '" onclick="accept(' +
+            i +
+            ')">Accept!</button><button class="rejectbtn" id="reject' +
+            i +
+            '" onclick="reject(' +
+            i +
+            ')">Reject</button></li></ul></ol>';
+          list.append(li);
+        } else {
+          //ngo
+          li.innerHTML =
+            "<ol>NGO : <ul><li>NAME : " +
+            details[i][0] +
+            "</li><li>ADDRESS : " +
+            details[i][1] +
+            "</li><li>CITY : " +
+            details[i][2] +
+            "</li><li>STATE : " +
+            details[i][3] +
+            "</li><li>PINCODE : " +
+            details[i][4] +
+            "</li><li>CONTACT : " +
+            details[i][5] +
+            "</li><li>DIFFICULTIES FACED BY NGO : " +
+            details[i][6] +
+            '</li><li><button class="documentsdownloadbtn" disabled>Download the Documents!</button><button class="acceptbtn" id="accept' +
+            i +
+            '" onclick="accept(' +
+            i +
+            ')">Accept!</button><button class="rejectbtn" id="reject' +
+            i +
+            '" onclick="reject(' +
+            i +
+            ')">Reject</button></li></ul></ol>';
+          list.append(li);
+        }
       }
-    }
-  };
+    };
+  }
 
-  function logout(){
-    localStorage.loggedinas="";
-    localStorage.isloggedin="false";
+  
+
+  function logout() {
+    localStorage.loggedinas = "";
+    localStorage.isloggedin = "false";
     window.location.replace("/HTML/index.html");
   }
 
-
-function accept(j){
-
-  document.getElementsByClassName("loginmessage")[0].textContent ="Are You Sure You Want To Proceed With The Approval?";
-  document.getElementById("messagebtn").textContent="OK!";
-  messagebg.classList.remove("invisible");
-  message.classList.remove("invisible");
-  document.onkeydown = function (e) {
-    return false;
-  };
-  document.getElementById("messagebtn").addEventListener("click",function(){
-    let verifieduser=localStorage.listdetails.split(",")[j];
-    let valueofverifieduser=localStorage.getItem("user"+verifieduser);
-    valueofverifieduser=valueofverifieduser.split(",");
-    valueofverifieduser[4]="true";
-    valueofverifieduser[5]="true";
-    if(valueofverifieduser[3]=="true"){
-      totaldonors++;
-      donors++;
-    }else{
-      totaldonors++;
-      ngos++;
-    }
-
-    localStorage.Totalusers=totalusers;
-    localStorage.Totalngos=ngos;
-    localStorage.Totaldonors=donors;
-    totalusers.textContent="Total Users = "+JSON.parse(localStorage.Totalusers);
-    ngos.textContent="NGOs = "+JSON.parse(localStorage.Totalngos);
-    donors.textContent="Donors = "+JSON.parse(localStorage.Totaldonors);
-
-    localStorage.setItem("user"+verifieduser,valueofverifieduser);
-    document.getElementById("accept"+j).classList.add("clicked");
-    document.getElementById("reject"+j).classList.add("clicked");
+  function accept(j) {
+    document.getElementsByClassName("loginmessage")[0].textContent =
+      "Are You Sure You Want To Proceed With The Approval?";
+    fetch.classList.add("invisible");
+    initialbtn.classList.add("invisible");
+    btn.classList.remove("invisible");
+    messagebg.classList.remove("invisible");
+    message.classList.remove("invisible");
     document.onkeydown = function (e) {
-      return true;
+      return false;
     };
-    messagebg.classList.add("invisible");
-    message.classList.add("invisible");
-  });
-}
+    btn.addEventListener("click", function () {
+      let verifieduser = localStorage.listdetails.split(",")[j];
+      let valueofverifieduser = localStorage.getItem("user" + verifieduser);
+      valueofverifieduser = valueofverifieduser.split(",");
+      valueofverifieduser[4] = "true";
+      valueofverifieduser[5] = "true";
 
+      // let u=0;
+      // let n=0;
+      // let d=0;
+      // u=JSON.parse(localStorage.Totalusers);
+      // n=JSON.parse(localStorage.Totalngos);
+      // d=JSON.parse(localStorage.Totaldonors);
 
-function reject(j){
+      // if (valueofverifieduser[2] === "true") {
+      //   d++;
+      // } else if(valueofverifieduser[2] === "false") {
+      //   n++;
+      // }
+      // u=n+d;
+      // localStorage.Totalusers = u;
+      // localStorage.Totalngos = n;
+      // localStorage.Totaldonors = d;
+      // totalusers.textContent ="Total Users = "+ JSON.parse(localStorage.Totalusers);
+      // ngos.textContent = "NGOs = " + JSON.parse(localStorage.Totalngos);
+      // donors.textContent = "Donors = " + JSON.parse(localStorage.Totaldonors);
 
+      localStorage.setItem("user" + verifieduser, valueofverifieduser);
+      document.getElementById("accept" + j).classList.add("clicked");
+      document.getElementById("reject" + j).classList.add("clicked");
+      document.onkeydown = function (e) {
+        return true;
+      };
+      messagebg.classList.add("invisible");
+      message.classList.add("invisible");
+    });
+  }
 
-
-  document.getElementsByClassName("loginmessage")[0].textContent ="Are You Sure You Want To Proceed With The Rejection?";
-  document.getElementById("messagebtn").textContent="OK!";
-  messagebg.classList.remove("invisible");
-  message.classList.remove("invisible");
-  document.onkeydown = function (e) {
-    return false;
-  };
-  document.getElementById("messagebtn").addEventListener("click",function(){
-    let verifieduser=localStorage.listdetails.split(",")[j];
-    let valueofverifieduser=localStorage.getItem("user"+verifieduser);
-    valueofverifieduser=valueofverifieduser.split(",");
-    valueofverifieduser[4]="true";
-    valueofverifieduser[6]="true";
-    localStorage.setItem("user"+verifieduser,valueofverifieduser);
-    document.getElementById("accept"+j).classList.add("clicked");
-    document.getElementById("reject"+j).classList.add("clicked");
+  function reject(j) {
+    document.getElementsByClassName("loginmessage")[0].textContent =
+      "Are You Sure You Want To Proceed With The Rejection?";
+    fetch.classList.add("invisible");
+    initialbtn.classList.add("invisible");
+    btn.classList.remove("invisible");
+    messagebg.classList.remove("invisible");
+    message.classList.remove("invisible");
     document.onkeydown = function (e) {
-      return true;
+      return false;
     };
-    messagebg.classList.add("invisible");
-    message.classList.add("invisible");
-  });
-}
-
+    btn.addEventListener("click", function () {
+      let verifieduser = localStorage.listdetails.split(",")[j];
+      let valueofverifieduser = localStorage.getItem("user" + verifieduser);
+      valueofverifieduser = valueofverifieduser.split(",");
+      valueofverifieduser[4] = "true";
+      valueofverifieduser[6] = "true";
+      localStorage.setItem("user" + verifieduser, valueofverifieduser);
+      document.getElementById("accept" + j).classList.add("clicked");
+      document.getElementById("reject" + j).classList.add("clicked");
+      document.onkeydown = function (e) {
+        return true;
+      };
+      messagebg.classList.add("invisible");
+      message.classList.add("invisible");
+    });
+  }
 }
